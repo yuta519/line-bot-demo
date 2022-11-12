a19342e2d2977c150b1dc192eaf181870552c66b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-11-12 23:17:09 +0900

    enable send line messageOC

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/lestrrat-go/iter v1.0.2 // indirect
 	github.com/lestrrat-go/jwx/v2 v2.0.6 // indirect
 	github.com/lestrrat-go/option v1.0.0 // indirect
-	github.com/line/line-bot-sdk-go/v7 v7.17.0 // indirect
+	github.com/line/line-bot-sdk-go/v7 v7.18.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"github.com/yuta519/line-bot-demo/infra"
 )
 
 func main() {
-	token := infra.FetchChannelAccessToken()
-	fmt.Println(token)
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		infra.FetchChannelAccessToken(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := linebot.NewTextMessage("hogehogehoge")
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
