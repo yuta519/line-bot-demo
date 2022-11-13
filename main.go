6f77d53da58f73bcf6a1316ca4be326d069d9901
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-11-13 22:25:40 +0900

    add handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/line/line-bot-sdk-go/v7/linebot"
@@ -18,10 +19,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	message := linebot.NewTextMessage("hogehogehoge")
+	message := linebot.NewTextMessage("MessagingAPIテスト")
 	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
 		log.Fatal(err)
 	}
 
 	infra.RevokeAccessToken(access_token)
+
+	http.HandleFunc("/", testHandler(bot, "aaaa"))
+	err = http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func testHandler(bot *linebot.Client, name string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hi, from Service: " + name))
+	}
 }
